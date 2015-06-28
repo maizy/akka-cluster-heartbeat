a99473961e3bf911976483ac6a86bab3cd59f699
Nikita Kovaliov <nikita@maizy.ru>
2015-06-28 20:41:34 +0300

    iss #3: add siblings broadcast pool, simple node events simulator

--- src/main/scala/ru/maizy/dev/heartbeat/HeartbeatNode.scala
+++ src/main/scala/ru/maizy/dev/heartbeat/HeartbeatNode.scala
@@ -1,7 +1,9 @@
 package ru.maizy.dev.heartbeat
 
-import akka.actor.{ Props, Actor }
+import scala.concurrent.duration._
+import akka.actor.{ ActorRef, Props, Actor, ActorLogging }
 import akka.event.{ LoggingReceive, Logging }
+import akka.routing._
 
 /**
  * Copyright (c) Nikita Kovaliov, maizy.ru, 2015
@@ -10,20 +12,48 @@ import akka.event.{ LoggingReceive, Logging }
 
 case object Beat
 case object GetHeartbeatStats
+case object BroadcastBeat
 case class HeartbeatStats(beatsReceived: BigInt)
 
-class HeartbeatNode extends Actor {
+case class AddSibling(node: ActorRef)
+case class RemoveSibling(node: ActorRef)
+//case object GetSiblings
 
-  val log = Logging(context.system, this)
+class HeartbeatNode extends Actor with ActorLogging {
+
   val printer = context.actorOf(Props[HeartbeatStatsPrinter], "printer")
+  val siblingNodesPool = context.actorOf(BroadcastPool(0).props(Props[HeartbeatNode]), "siblings-pool")
+
+  import context.dispatcher
+  val schedule = context.system.scheduler.schedule(
+    initialDelay = 0.seconds,
+    interval = 1.second,
+    self,
+    BroadcastBeat)
+
+  override def postStop() {
+    schedule.cancel()
+  }
 
   var received: BigInt = 0
 
   def receive: Receive = LoggingReceive {  // LoggingReceive decorator used for debug only
+
     case Beat =>
       received += 1
       printer ! stats
+
     case GetHeartbeatStats => sender ! stats
+
+    case BroadcastBeat => siblingNodesPool ! Broadcast(Beat)
+
+    case AddSibling(node: ActorRef) =>
+      log.info(s"add sibling ${node.path}")
+      siblingNodesPool ! AddRoutee(ActorRefRoutee(node))
+
+    case RemoveSibling(node: ActorRef) =>
+      log.info(s"remove sibling ${node.path}")
+      siblingNodesPool ! RemoveRoutee(ActorRefRoutee(node))
   }
 
   private def stats(): HeartbeatStats = HeartbeatStats(received)
--- src/main/scala/ru/maizy/dev/heartbeat/Main.scala
+++ src/main/scala/ru/maizy/dev/heartbeat/Main.scala
@@ -1,9 +1,11 @@
 package ru.maizy.dev.heartbeat
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
-import akka.actor.{ ActorRef, ActorSystem, Props }
 import com.typesafe.config.ConfigFactory
+import akka.actor.{ PoisonPill, ActorSystem, Props }
+import akka.util.Timeout
 
 /**
  * Copyright (c) Nikita Kovaliov, maizy.ru, 2015
@@ -17,11 +19,60 @@ object Main extends App {
 
   val dispatcher = system.dispatcher
   implicit val executionContext = system.dispatcher
+  implicit val defaultTimeout = Timeout(500.millis)  //TODO: from config
 
-  //FIXME: tmp
-  val node1 = system.actorOf(Props[HeartbeatNode], "node1")
-  val node2 = system.actorOf(Props[HeartbeatNode], "node2")
 
-  system.scheduler.schedule(initialDelay = 0.seconds, interval = 1.second, node2, Beat)(dispatcher, node1)
-  system.scheduler.schedule(initialDelay = 1.seconds, interval = 2.second, node1, Beat)(dispatcher, node2)
+  //Emulate nodes events
+
+  def emulateEventSeq(events: List[() => Any], _n: Int = 0): Unit = events match {
+    case event :: tail =>
+      system.log.info(s"emulate event #${_n}")
+      event()
+      system.scheduler.scheduleOnce(10.second)(emulateEventSeq(tail, _n + 1))
+    case _ =>
+      system.log.info("no more events to emulate, wait 10 seconds and shutdown")
+      system.scheduler.scheduleOnce(10.second)(system.shutdown())
+  }
+
+  val reportError: PartialFunction[Throwable, Unit] = {
+    case e => system.log.warning(s"oops: $e")
+  }
+
+  def reportErrorOrApply[T](future: Future[T])(func: PartialFunction[T, Unit]) {
+    future onSuccess func
+    future onFailure reportError
+  }
+
+  val events = List(
+    () => {
+      system.log.info("EVENT: add node1")
+      system.actorOf(Props[HeartbeatNode], "node1")
+    },
+    () => {
+      system.log.info("EVENT: add node2, connect nodes")
+      for (node1 <- system.actorSelection("user/node1").resolveOne) {
+          val node2 = system.actorOf(Props[HeartbeatNode], "node2")
+          node2 ! AddSibling(node1)
+          node1 ! AddSibling(node2)
+      }
+    },
+    () => {
+      system.log.info("EVENT: remove node1")
+      for (
+        node1 <- system.actorSelection("user/node1").resolveOne;
+        node2 <- system.actorSelection("user/node2").resolveOne
+      ) {
+        node2 ! RemoveSibling(node1)
+        node1 ! PoisonPill
+      }
+    },
+    () => {
+      system.log.info("EVENT: remove node2")
+      for (node2 <- system.actorSelection("user/node2").resolveOne) {
+        node2 ! PoisonPill
+      }
+    }
+  )
+
+  emulateEventSeq(events)
 }
